e50a0d43e35100486b4434f6ee0e8268a91d7dbe
Phill Barron <Phill.Barron@bbc.co.uk>
2019-03-08 08:42:00 +0000

    bunch of debug but actually getting appropriate responses now. Having to manually build the list of credentials providers which feels wrong but perhaps this is how it's done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 const wormholeClient = require("@bbc/cps-wormhole");
 const AWS = require("aws-sdk");
 
+const foundCredentials = async (error, credentials) => {
+  if (error) return new Error(err);
+  if (credentials.then) {
+    credentials.then(resolvedCredentials => {
+      return console.log(
+        "the first valid credentials we found were",
+        resolvedCredentials
+      );
+    });
+  } else {
+    console.log("the first valid credentials we found were", credentials);
+  }
+};
+
 const configureCredentials = async () => {
   const awsCredentialsProvider = async () =>
     new AWS.Credentials(await wormholeClient.getCredentials());
-  console.log({ awsCredentialsProvider });
-  const chain = new AWS.CredentialProviderChain();
-  chain.providers.push(awsCredentialsProvider);
+  const environemtnCredentialsAWS = () => new AWS.EnvironmentCredentials("AWS");
+  const environemtnCredentialsAMAZON = () =>
+    new AWS.EnvironmentCredentials("AMAZON");
+  console.log({
+    awsCredentialsProvider
+  });
+  const chain = new AWS.CredentialProviderChain([
+    environemtnCredentialsAWS,
+    environemtnCredentialsAMAZON,
+    awsCredentialsProvider
+  ]);
   console.log(chain.providers);
-  const credentials = await chain.resolvePromise();
-  console.log("resolved credentials", credentials);
+  const resolvedCreds = await chain.resolvePromise(foundCredentials);
+  console.log("resolvedCreds", resolvedCreds);
 };
 
 configureCredentials();
